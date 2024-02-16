d36b008607ca2e2ceb54a5f6bfee8547fc9ba390
jack <redcatmiss@126.com>
2024-02-17 07:02:17 +0800

    service test framework and test config file

--- internal/logic/vat_gateway/vatsense/vatsense_test.go
+++ internal/logic/vat_gateway/vatsense/vatsense_test.go
@@ -1,11 +1,7 @@
 package vatsense
 
-import (
-	"testing"
-)
-
 var apiKey = "d9d57f2212cba7e286b3fb9cbb2ad419"
 
-func Test(t *testing.T) {
-	//fmt.Println(ListAllCountries())
-}
+//func Test(t *testing.T) {
+//	//fmt.Println(ListAllCountries())
+//}
